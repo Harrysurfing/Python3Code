c6e73317189aaea07f2488f84979665d6f5d80f6
System Administrator <root@Pinzhis-MacBook-Pro.local>
2020-02-24 16:40:03 +1100

    hacker rank swap case practise

---
+++ swapcase.py
@@ -0,0 +1,15 @@
+#swap case for a string
+
+def swap_case(s):
+    result = ""
+    for i in s:
+        if i.isupper():
+            result += i.lower()
+        else:
+            result += i.upper()
+    return result
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
